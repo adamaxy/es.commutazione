1a969c29d369b2165d2195b00206c27f49211a4b
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-07-21 14:02:38 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,22 @@
 public class main {
     public static void main(String[] args) {
-     String numberList = "5";
-     switch (numberList){
-         case "1" -> System.out.println("one");
-         case "2" -> System.out.println("two");
-         case "3" -> System.out.println("three");
-         case "4" -> System.out.println("four");
-         case "5" -> System.out.println("five");
-         case "6" -> System.out.println("six");
-         case "7" -> System.out.println("seven");
-         case "8" -> System.out.println("eight");
-         case "9" -> System.out.println("nine");
-         case "10" -> System.out.println("ten");
-         default -> System.out.println("Cannot give you the name");
-     }
-}
+
+        String imputUtente = "5";
+        String numberList = imputUtente;
+        switch (numberList){
+            case "1" -> System.out.println("one");
+            case "2" -> System.out.println("two");
+            case "3" -> System.out.println("three");
+            case "4" -> System.out.println("four");
+            case "5" -> System.out.println("five");
+            case "6" -> System.out.println("six");
+            case "7" -> System.out.println("seven");
+            case "8" -> System.out.println("eight");
+            case "9" -> System.out.println("nine");
+            case "10" -> System.out.println("ten");
+            default -> System.out.println("Cannot give you the name");
+
+
+        }
+    }
 }
