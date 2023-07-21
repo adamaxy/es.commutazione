3806f39b6eff8e272515d619ce4ea77c3ba50e4c
adamaxy <adam996@libero.it>
2023-07-21 08:43:32 +0200

    aggiunta esercizio

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+     String numberList = "5";
+     switch (numberList){
+         case "1" -> System.out.println("one");
+         case "2" -> System.out.println("two");
+         case "3" -> System.out.println("three");
+         case "4" -> System.out.println("four");
+         case "5" -> System.out.println("five");
+         case "6" -> System.out.println("six");
+         case "7" -> System.out.println("seven");
+         case "8" -> System.out.println("eight");
+         case "9" -> System.out.println("nine");
+         case "10" -> System.out.println("ten");
+         default -> System.out.println("Cannot give you the name");
+     }
+}
+}
